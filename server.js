c5ea63060d807b40a1411e707680056f9bf9496e
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2022-12-24 22:51:44 +0300

    adding some few things on html page

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require ('express')
 const app =express ()
 
-app.get('/', (req, res) => {
+app.set('view-engine', 'ejs')
 
+app.get('/', (req, res) => {
+    res.render('index.js')
 })
 app.listen(3000)
